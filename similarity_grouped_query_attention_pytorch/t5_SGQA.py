49055e64ed14d9f648a06deeddc75fabcdfd80aa
saisena1998 <saisena314@gmail.com>
2023-11-30 12:17:34 -0500

    updated from translate to summarize

--- similarity_grouped_query_attention_pytorch/t5_SGQA.py
+++ similarity_grouped_query_attention_pytorch/t5_SGQA.py
@@ -270,8 +270,22 @@ def convert_t5_to_gqa(module, kv_heads: int,similarity_flag:bool=False,inplace: 
 if __name__=='__main__':
     t5: T5ForConditionalGeneration = T5ForConditionalGeneration.from_pretrained("t5-small")
     tokenizer = T5Tokenizer.from_pretrained("t5-small", legacy=False)
-    input_ids = tokenizer("translate English to German: The house is wonderful.", return_tensors="pt").input_ids
+    summ_text = '''After the sound and the fury, weeks of demonstrations and anguished calls for racial justice, t
+                he man whose death gave rise to an international movement, and whose last words — “I can’t breathe” — have 
+                been a rallying cry, will be laid to rest on Tuesday at a private funeral in Houston.George Floyd, who was 46, 
+                will then be buried in a grave next to his mother’s.The service, scheduled to begin at 11 a.m. at the Fountain of 
+                Praise church, comes after five days of public memorials in Minneapolis, North Carolina and Houston and two weeks 
+                after a Minneapolis police officer was caught on video pressing his knee into Mr. Floyd’s neck for nearly nine minutes 
+                before Mr. Floyd died. That officer, Derek Chauvin, has been charged with second-degree murder and second-degree 
+                manslaughter. His bail was set at $1.25 million in a court appearance on Monday. The outpouring of anger and outrage 
+                after Mr. Floyd’s death — and the speed at which protests spread from tense, chaotic demonstrations in the city where 
+                he died to an international movement from Rome to Rio de Janeiro — has reflected the depth of frustration borne of 
+                years of watching black people die at the hands of the police or vigilantes while calls for change went unmet.', 80'''
 
+    input_ids = tokenizer("summarize:"+summ_text, return_tensors="pt").input_ids
+    outputs = t5.generate(input_ids, max_new_tokens=25)
+    text = tokenizer.batch_decode(outputs[0], skip_special_tokens=True)
+    print(f'Generated text with pretrained model: {text}')
     #convert t5 to gqa
     for kv_heads in [8,4]:
         for similarity_flag in [False,True]:
