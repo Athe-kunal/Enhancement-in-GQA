9c2b95fd94876da3faabc076ba24478603c320aa
Astarag Mohapatra <40949756+Athe-kunal@users.noreply.github.com>
2023-12-05 02:33:17 -0800

    Update config.py

--- similarity_grouped_query_attention_pytorch/config.py
+++ similarity_grouped_query_attention_pytorch/config.py
@@ -4,9 +4,10 @@ MODEL_NAME = "t5-small"
 BATCH_SIZE = 8
 NUM_EPOCHS = 10
 LEARNING_RATE = 5e-5
-GQA_LIST = ['encoder','decoder','EncDecAttention']
+GQA_LIST = ['decoder','EncDecAttention']
 WANDB_API_KEY = "70e95a405ea4aec8d0a637460407bf21c69436f4"
 WANDB_PROJECT = "similarity_gqa"
 WANDB_ENTITY = "athe_kunal"
 VAL_BATCH_SIZE = 64
+IF_RANDOM=False
 
