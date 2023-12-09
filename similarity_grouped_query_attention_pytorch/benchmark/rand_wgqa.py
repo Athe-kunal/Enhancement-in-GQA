272970394bcd793460e080c1307885c9c64f82b8
saisena1998 <saisena314@gmail.com>
2023-12-09 10:48:26 -0500

    checkpoint results script

--- similarity_grouped_query_attention_pytorch/benchmark/rand_wgqa.py
+++ similarity_grouped_query_attention_pytorch/benchmark/rand_wgqa.py
@@ -19,7 +19,7 @@ def cleanup():
 
 def main(rank, world_size,run):
     setup(rank, world_size)
-    val_rouge_dict, test_rouge_dict = train(rank,world_size,kv_heads=4,logging_name="rand_wgqa",run=run,model_name=config.MODEL_NAME,similarity_flag=False,weight_flag=True,if_random=True)
+    val_rouge_dict, test_rouge_dict = train(rank,world_size,kv_heads=4,logging_name="randwgqa",run=run,model_name=config.MODEL_NAME,similarity_flag=False,weight_flag=True,if_random=True)
     print(f'validation rogue dict:{val_rouge_dict}')
     print(f'Test rogue dict:{test_rouge_dict}')
     cleanup()
